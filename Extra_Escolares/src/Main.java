a0b944cecfca92bec50d8005b7c74bc0d413ef0e
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-03-24 14:45:22 -0700

    commit

---
+++ Extra_Escolares/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz_Principal IP = new Interfaz_Principal();
+        IP.setVisible(true);
+    }
+    
+}
