1b8fef94e11c59bacc5eed420aada6c828993782
Samuel Salgado Gonzalez <vesuvianmetal@hotmail.com>
2017-03-24 22:50:58 -0700

    commit

--- Extra_Escolares/src/Interfaz_Principal.java
+++ Extra_Escolares/src/Interfaz_Principal.java
@@ -1,3 +1,7 @@
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -162,7 +166,13 @@ public class Interfaz_Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnbuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnbuscarActionPerformed
-       
+JFileChooser fileChooser = new JFileChooser();
+fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+int result = fileChooser.showOpenDialog(this);
+if (result == JFileChooser.APPROVE_OPTION) {
+    File selectedFile = fileChooser.getSelectedFile();
+    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+}       
     }//GEN-LAST:event_btnbuscarActionPerformed
 
     /**
